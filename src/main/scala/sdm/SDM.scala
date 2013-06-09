eabbd6d3cae185ead6914c492f0f07612ab64d15
Tom Green <tmgreen@broadinstitute.org>
2013-06-09 13:47:35 -0400

    better time reporting, add 8cells.out

--- src/main/scala/sdm/SDM.scala
+++ src/main/scala/sdm/SDM.scala
@@ -1,12 +1,8 @@
 package sdm
 
-import math.{ ceil, log }
-import scala.collection.mutable.ListBuffer
 import scala.collection.Set
 import scala.collection.mutable
 import scala.concurrent._
-import ExecutionContext.Implicits.global
-import scala.collection.SortedSet
 
 /** The main command-line interface.
   *
@@ -20,16 +16,25 @@ object SDM {
     val ncells = args(0).toInt
     implicit val theory = Theory(ncells)
     import theory._
-    
+
     println(ncells + " cells in model.")
     println(totalFeatures + " total features available.")
     println(s"$featuresNeeded features needed for the $ncells cell case.")
+    val allStartTime = System.currentTimeMillis()
 
-    case class Result(count: Int, goodCount: Int, profiles: Set[Set[Long]]) {
-      override def toString = s"$count\t$goodCount\t${profiles.size}"
+    case class Result(count: Int, goodCount: Int, profiles: Set[Set[Long]], elapsedMillis: Int) {
+      override def toString = s"$count\t$goodCount\t${profiles.size}\t${timeString(elapsedMillis)}"
+
+      def +(r2: Result): Result =
+        Result(
+          count = count + r2.count,
+          goodCount = goodCount + r2.goodCount,
+          profiles = profiles ++ r2.profiles,
+          elapsedMillis = elapsedMillis + r2.elapsedMillis)
     }
 
     def handleSpace(rootMask: Int): Result = {
+      val startTime = System.currentTimeMillis()
       val mpartProfiles: mutable.Set[Set[Long]] = mutable.Set.empty
       var count = 0
       var ngood = 0
@@ -45,22 +50,32 @@ object SDM {
       val space = FeatureSetSpace(rootMask)
       space.foreach(handleFs)
 
-      val res = Result(count, ngood, mpartProfiles)
+      val elapsed = System.currentTimeMillis() - startTime
+      val res = Result(count, ngood, mpartProfiles, elapsed.toInt)
       println(s"$rootMask\t$res")
       res
     }
 
-    def reducer(r1: Result, r2: Result): Result = {
-      Result(count = r1.count + r2.count,
-        goodCount = r1.goodCount + r2.goodCount,
-        profiles = r1.profiles ++ r2.profiles)
-    }
-    
+    def reducer(r1: Result, r2: Result): Result = { r1 + r2 }
+
     val finalResult = (1 to (totalFeatures - featuresNeeded + 1)).par map handleSpace reduce reducer
-    
+    val allElapsedMillis = System.currentTimeMillis() - allStartTime
+
     println("Total featuresets considered: " + finalResult.count)
     println("Total fine featuresets: " + finalResult.goodCount)
     println("Total paradigm sets: " + finalResult.profiles.size)
+    println("Total elapsed compute time: " + timeString(finalResult.elapsedMillis))
+    println("Total elapsed actual time: " + timeString(allElapsedMillis))
+  }
+
+  def timeString(millis: Long): String = {
+    if (millis < 2000) {
+      millis + "ms"
+    } else if (millis < 120000) {
+      (millis / 1000) + "s"
+    } else {
+      (millis / 60000) + "m"
+    }
   }
 
   def tallyParadigms(fs: FeatureSet, profiles: mutable.Set[Set[Long]]) {
